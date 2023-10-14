a7966b26ef9e432de233a1d49bff675a18da5df6
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2023-10-14 11:11:50 +0200

    Simulating adding new score entities

--- src/PremierLeague.jsx
+++ src/PremierLeague.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import PremierLeagueScores from "./PremierLeagueScores";
 
 const API_URL = '/scores';
+const headers = {
+  'Content-Type': 'application/json',
+};
 
 function PremierLeague() {
     const [data, setData] = useState([]);
-    const [error, setError] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
         fetchData();
@@ -18,11 +21,28 @@ function PremierLeague() {
       .then(data=>setData(data))
       .catch(error=>setError(error));
     };
+
+    const handleCreate = (item) => {
+      console.log('add item: ${JSON.stringify(item)}')
+      console.log("  " + item.name + " " + item.B3up + " " + item.Uno + " " + item.FootballDice + " " + item.Darts)
+
+      fetch(API_URL, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({id: item.id, name: item.name, B3up: item.B3up, Uno: item.Uno, FootballDice: item.FootballDice, Darts: item.Darts}),
+      })
+      .then(response => response.json())
+      .then(returnedItem => setData([...data, returnedItem]))
+      .catch(error => setError(error + "  " + item.name + " " + item.B3up + " " + item.Uno + " " + item.FootballDice + " " + item.Darts));
+    };
  
 
   return (
       <PremierLeagueScores
-          data={data}/>
+          data={data}
+          error={error}
+          onCreate={handleCreate}
+          />
   );
   }
 
--- src/PremierLeagueScores.jsx
+++ src/PremierLeagueScores.jsx
@@ -1,18 +1,48 @@
-export default function PremierLeagueScores({data}){
-    return(
-        <div>
-            <ul>
-                {
-                data.map((player, index)=>(
-                    <li key={index}>
-                        {player.name}: 
-                            {player.scores.B3up} // 
-                            {player.scores.Uno} // 
-                            {player.scores.FootbalDice} // 
-                            {player.scores.Darts} //
-                    </li>
-                ))}
-            </ul>
-        </div>
+import { useState } from "react";
+
+export default function PremierLeagueScores({data, onCreate, error}){
+    const [formData, setFormData] = useState({id:'', name:'', B3up:'', Uno:'', FootballDice:'', Darts:''});
+
+
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevData => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(formData);
+        setFormData({id:'', name:'', B3up:'', Uno:'', FootballDice:'', Darts:'' });
+      };
+
+    return (
+      <div>
+        <h2>New</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="name" onChange={handleFormChange}/><br/>
+          <input type="text" name="B3up" placeholder="B3UP" onChange={handleFormChange}/><br/>
+          <input type="text" name="Uno" placeholder="UNO" onChange={handleFormChange}/><br/>
+          <input type="text" name="FootballDice" placeholder="Footbal Dice" onChange={handleFormChange}/><br/>
+          <input type="text" name="Darts" placeholder="Darts" onChange={handleFormChange}/><br/>
+          <button type="submit">
+            Create
+          </button>
+        </form>
+
+        <ul>
+          {data.map((player, index) => (
+            <li key={index}>
+              {player.name}:{player.B3up} //
+              {player.Uno} //
+              {player.FootballDice} //
+              {player.Darts} //
+            </li>
+          ))}
+        </ul>
+        {error && <div>{error.message}</div>}
+      </div>
     );
 }
